1c9c5f1f6c7c6a9825a35ae1f1ef7dfaf4b4304e
Asish Singh <Aashishsingh1317@gmail.com>
2025-08-28 02:37:52 +0530

    Added the screens , bottom naviagtion , components with backend

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,49 @@
-import { Stack } from "expo-router";
-import "./global.css";
-import { useFonts } from "expo-font";
-import { useEffect } from "react";
-import * as SplashScreen from 'expo-splash-screen';
+import {SplashScreen, Stack} from "expo-router";
+import { useFonts } from 'expo-font';
+import { useEffect} from "react";
 
-// Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
+import * as Sentry from '@sentry/react-native';
+import useAuthStore from "@/store/auth.store";
 
-export default function RootLayout() {
-  const [fontsLoaded, fontError] = useFonts({
-    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
-    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
-    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
-    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
-    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf")
+Sentry.init({
+  dsn: 'https://94edd17ee98a307f2d85d750574c454a@o4506876178464768.ingest.us.sentry.io/4509588544094208',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
+  const { isLoading, fetchAuthenticatedUser } = useAuthStore();
+
+  const [fontsLoaded, error] = useFonts({
+    "QuickSand-Bold": require('../assets/fonts/Quicksand-Bold.ttf'),
+    "QuickSand-Medium": require('../assets/fonts/Quicksand-Medium.ttf'),
+    "QuickSand-Regular": require('../assets/fonts/Quicksand-Regular.ttf'),
+    "QuickSand-SemiBold": require('../assets/fonts/Quicksand-SemiBold.ttf'),
+    "QuickSand-Light": require('../assets/fonts/Quicksand-Light.ttf'),
   });
 
   useEffect(() => {
-    if (fontsLoaded || fontError) {
-      // Hide splash screen once fonts are loaded or if there's an error
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
+    if(error) throw error;
+    if(fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded, error]);
 
-  // Don't render the app until fonts are loaded
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
+  useEffect(() => {
+    fetchAuthenticatedUser()
+  }, []);
 
-  return <Stack />;
-}
+  if(!fontsLoaded || isLoading) return null;
+
+  return <Stack screenOptions={{ headerShown: false }} />;
+});
+
+Sentry.showFeedbackWidget();
